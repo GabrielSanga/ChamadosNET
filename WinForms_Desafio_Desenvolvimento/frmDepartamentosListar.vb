5e8fa20d48e1b9eb50e8da1801831d65f9a585f7
GabrielSanga <gabriel.sanga.fef@gmail.com>
2025-03-15 15:50:29 -0300

    Adicionando projeto

---
+++ WinForms_Desafio_Desenvolvimento/frmDepartamentosListar.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Data
+Imports System.Windows.Forms
+
+Public Class frmDepartamentosListar
+
+    Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim dtDepartamentos As DataTable = Dados.ListarDepartamentos()
+        Me.dgvDepartamentos.DataSource = dtDepartamentos
+
+    End Sub
+
+    Private Sub btnExcluir_Click(sender As Object, e As EventArgs) Handles btnExcluir.Click
+
+
+    End Sub
+
+    Private Sub btnAbrir_Click(sender As Object, e As EventArgs) Handles btnAbrir.Click
+
+
+    End Sub
+
+    Private Sub btnNovo_Click(sender As Object, e As EventArgs) Handles btnNovo.Click
+
+
+    End Sub
+
+    Private Sub btnRelatorio_Click(sender As Object, e As EventArgs) Handles btnRelatorio.Click
+
+        Dim frm As New frmDepartamentosRelatorio()
+        frm.ShowDialog()
+
+    End Sub
+End Class
---
+++ WinForms_Desafio_Desenvolvimento/frmDepartamentosRelatorio.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Windows.Forms
+Imports Microsoft.Reporting.WinForms
+
+Public Class frmDepartamentosRelatorio
+    Private Sub frmDepartamentosRelatorio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.ReportViewer1.RefreshReport()
+
+    End Sub
+End Class
